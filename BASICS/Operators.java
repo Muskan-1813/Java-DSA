a87c14ad2572a6fe448f2f1c94230b2d2a13d1ff
Muskan Kumari <muskan987654kumari@gmail.com>
2025-11-02 15:34:47 +0530

    Day 02 Added

---
+++ BASICS/Operators.java
@@ -0,0 +1,54 @@
+public class Operators{
+    public static void main (String args[]){
+          int A = 10;
+          int B = 5;
+        System.out.println("add="+(A+B));
+        System.out.println("sub="+(A-B));
+        System.out.println("mul="+(A*B));
+        System.out.println("div="+(A/B));
+
+        //INCREMENT OPERATOR
+        int a = 5;
+        int b =++a;
+        System.out.println(a);
+        System.out.println(b);
+
+        int a1 = 5;
+        int b1 =a1++;
+        System.out.println(a1);
+        System.out.println(b1);
+
+        //DECREMENT OPERATOR
+        int a2 = 5;
+        int b2 =--a2;
+        System.out.println(a2);
+        System.out.println(b2);
+
+        int a3 = 5;
+        int b3 =a3--;
+        System.out.println(a3);
+        System.out.println(b3);
+
+        //RELATIONAL OPERATORS
+        int X=5 ;
+        int Y=6 ;
+        System.out.println(X==Y);
+        System.out.println(X!=Y);
+        System.out.println(X>Y);
+        System.out.println(X<Y);
+        System.out.println(X>=Y);
+        System.out.println(X<=Y);
+
+        //LOGICAL OPERATORS
+        System.out.println((3>2)&&(5>0));
+        System.out.println((3<2)||(5>0));
+        System.out.println(!(3<2));
+
+        //ASSIGNMENT OPERATORS
+        int R=5;
+        System.out.println(R);
+        System.out.println(R+=1);
+        System.out.println(R-=1);
+        System.out.println(R*=1);
+    }
+}
