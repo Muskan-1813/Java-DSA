533d54946f6f873fb194d600427bec9d2e1dd7cd
Muskan Kumari <muskan987654kumari@gmail.com>
2025-11-09 23:36:11 +0530

    Removed a file

---
+++ BASICS/Advanced_Functions.java
@@ -0,0 +1,3 @@
+public class Advanced_Functions {
+    
+}
--- BASICS/Functions.java
+++ BASICS/Functions.java
@@ -1,8 +1,20 @@
+import java.util.*;
 public class Functions{
     public static void printHello(){
         System.out.println("Hello Guyz!!");
     }
+
+    public static int sum(int a, int b){
+        int sum = a +b;
+        return sum ;
+    }
     public static void main(String[] args) {
         printHello();
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = sum(a,b);
+        System.out.println("Sum is:"+result);
+
     }
-}
+}                                                     
